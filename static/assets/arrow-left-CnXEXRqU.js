932b7f9da84cc21a4f1da133be3b6e2ea65b94fb
ShianMike <22ur0682@psu.edu.ph>
2026-03-05 10:46:56 +0800

    fix: set VITE_API_URL for production builds to Cloud Run backend

--- static/assets/arrow-left-CnXEXRqU.js
+++ static/assets/arrow-left-CnXEXRqU.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-DoWNgqBj.js";/**
+import{c as o}from"./index-DC9Ncvim.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
